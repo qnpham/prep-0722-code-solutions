fe07609fc0566b9b6b4b8e2969e1b47afdf091f8
Quan Pham <mockanterr@gmail.com>
2022-08-10 06:29:35 +0000

    completed prep-javascript-for-loops exercise

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,10 @@
+// create your loops here.
+for (var i = 0; i <= 9; i++) {
+  console.log(i);
+}
+for (var i = 0; i <= 9; i++) {
+  console.log(i * 2);
+}
+for (var i = 100; i > 0; i--) {
+  console.log('Time till explosion ' + i + '!');
+}
